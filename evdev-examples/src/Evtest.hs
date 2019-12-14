a72714881a9d315bd25f61ce5d6342ba1c408fc5
George Thomas <george.thomas@drisq.com>
2019-12-14 13:40:43 +0000

    Add examples.
    Individual files have not been changed, just moved to a subdirectory.

---
+++ evdev-examples/src/Evtest.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import qualified Data.ByteString.Char8 as BS
+import Evdev
+import Evdev.Stream
+import qualified Streamly.Prelude as S
+import Streamly 
+
+main :: IO ()
+main = do
+    forM' (makeDevices allDevicePaths) $ \dev -> do
+        let path = devicePath dev
+        name <- getDeviceName dev
+        BS.putStrLn $ path <> ":" <> BS.replicate (24 - BS.length path) ' ' <> name
+    BS.putStr "Choose device number (s): "
+    ns <- BS.words <$> BS.getLine
+    let paths = S.fromFoldable $ map (\n -> evdevDir <> "/event" <> n) ns
+    forM' (readEventsMany $ makeDevices paths) $ \(d,e) ->
+        BS.putStrLn $ devicePath d <> ": " <> BS.pack (prettyEvent e)
+
+forM' :: Monad m => SerialT m a -> (a -> m b) -> m ()
+forM' = flip S.mapM_
