6d6b92ff6702f4494893641c588c296f2344bc02
George Thomas <george.thomas@drisq.com>
2019-12-15 16:06:33 +0000

    Haddock info for Stream module.

--- evdev/src/Evdev/Stream.hs
+++ evdev/src/Evdev/Stream.hs
@@ -20,25 +20,26 @@ import qualified Streamly.Prelude as S
 import Evdev
 
 
--- read all events from a device
+-- | Read all events from a device.
 readEvents :: Device -> SerialT IO Event
 readEvents dev = S.repeatM $ nextEvent dev defaultReadFlags
 
--- concurrently read events from multiple devices
+-- | Concurrently read events from multiple devices.
 readEventsMany :: IsStream t => AsyncT IO Device -> t IO (Device, Event)
 readEventsMany ds = asyncly $ do
     d <- ds
     S.map (d,) $ serially $ readEvents d
 
--- will throw an exception if a path doesn't correspond to a valid input device
+-- | Create devices for all paths in the stream.
+-- | Will throw an exception if a path doesn't correspond to a valid input device.
 makeDevices :: IsStream t => t IO RawFilePath -> t IO Device
 makeDevices = S.mapM newDevice
 
--- all events on all valid devices
+-- | All events on all valid devices (in /dev/input).
 allEvents :: IsStream t => t IO (Device, Event)
 allEvents = readEventsMany allDevices
 
--- all paths in evdevDir corresponding to valid input devices
+-- | All valid devices (in /dev/input).
 allDevices :: (IsStream t, Monad (t IO)) => t IO Device
 allDevices =
     let maybeNewDevice path = catchJust (guard . isIllegalOperation) (Just <$> newDevice path) (\() -> return Nothing)
