c969916dea05b58fdf684f3879396ec068ace38b
George Thomas <george.thomas@drisq.com>
2019-12-15 16:24:17 +0000

    Add helpful output to Evtest.

--- evdev-examples/src/Evtest.hs
+++ evdev-examples/src/Evtest.hs
@@ -14,7 +14,7 @@ main = do
         let path = devicePath dev
         name <- deviceName dev
         BS.putStrLn $ path <> ":" <> BS.replicate (24 - BS.length path) ' ' <> name
-    BS.putStr "Choose device number (s): "
+    BS.putStr "Choose device numbers (separated by whitespace): "
     ns <- BS.words <$> BS.getLine
     let paths = S.fromFoldable $ map ((evdevDir <> "/event") <>) ns
     forM' (readEventsMany $ makeDevices paths) $ \(d,e) ->
