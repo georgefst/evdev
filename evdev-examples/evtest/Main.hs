6dd7108a23ef1d02440d4123e7a6dd83ea97a779
George Thomas <georgefsthomas@gmail.com>
2020-07-25 15:08:15 +0100

    Add option to evtest to watch all new devices

--- evdev-examples/evtest/Main.hs
+++ evdev-examples/evtest/Main.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE LambdaCase #-}
 module Main (main) where
 
 import qualified Data.ByteString.Char8 as BS
+import System.Environment (getArgs)
 import Text.Pretty.Simple (pPrint)
 
 import qualified Streamly.Prelude as S
@@ -9,12 +11,22 @@ import Evdev
 import Evdev.Stream
 
 main :: IO ()
-main = do
-    S.mapM_ printDevice allDevices
-    BS.putStr "Choose device numbers (separated by whitespace): "
-    ns <- BS.words <$> BS.getLine
-    let paths = S.fromFoldable $ map ((evdevDir <> "/event") <>) ns
-    S.mapM_ pPrint $ readEventsMany $ makeDevices paths
+main = getArgs >>= \case
+    ["new"] -> do
+        let printNewDevice d = do
+                BS.putStrLn ""
+                BS.putStrLn "New device:"
+                BS.putStrLn $ devicePath d
+                BS.putStrLn =<< deviceName d
+                BS.putStrLn ""
+                return d
+        S.mapM_ pPrint $ readEventsMany $ S.mapM printNewDevice newDevices
+    _ -> do
+        S.mapM_ printDevice allDevices
+        BS.putStr "Choose device numbers (separated by whitespace): "
+        ns <- BS.words <$> BS.getLine
+        let paths = S.fromFoldable $ map ((evdevDir <> "/event") <>) ns
+        S.mapM_ pPrint $ readEventsMany $ makeDevices paths
 
 printDevice :: Device -> IO ()
 printDevice dev = do
