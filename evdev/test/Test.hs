f7eafd5b9e97975caee2529a98b1edd13db1ffca
George Thomas <georgefsthomas@gmail.com>
2021-02-11 22:45:30 +0000

    Add quickcheck tests that conversions are inverses

--- evdev/test/Test.hs
+++ evdev/test/Test.hs
@@ -8,6 +8,7 @@ import Data.Either.Extra
 import Data.Foldable.Extra
 import Data.Functor
 import Data.Maybe
+import Data.Time
 import Evdev
 import Evdev.Codes
 import Evdev.Uinput
@@ -16,17 +17,21 @@ import System.FilePath.ByteString
 import System.IO.Error
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+
+main :: IO ()
+main = defaultMain $ testGroup "Tests" [smoke, inverses]
 
 {-TODO
 doesn't work relably in GHCI
     if we fail to get a device in any run after the first, we just hang for some reason
 -}
 
-{- | Currently just checks that we can create a virtual device, find it, see that it has the expected properties,
+{- | Just checks that we can create a virtual device, find it, see that it has the expected properties,
 and read the expected events.
 -}
-main :: IO ()
-main = do
+smoke :: TestTree
+smoke = testCase "Smoke" do
     start <- newEmptyMVar
     let duName = "evdev-test-device"
         keys = [Key1 .. Key0]
@@ -35,17 +40,43 @@ main = do
     void $ forkIO do
         takeMVar start -- wait until reading device is initialised
         writeBatch du evs
-    defaultMain . testCase "Test 1" $
-        listDirectory evdevDir
-            >>= traverse (fmap eitherToMaybe . try @IOError . (retryIf isPermissionError . newDevice) . (evdevDir </>))
-            >>= findM (fmap (== duName) . deviceName) . catMaybes
-            >>= \case
-                Nothing -> assertFailure "Couldn't find device with correct name"
-                Just d -> do
-                    putMVar start ()
-                    (@?= [EvSyn, EvKey]) =<< deviceEventTypes d
-                    evs' <- whileJust ((\x -> guard (x /= last evs) $> x) . eventData <$> nextEvent d) pure
-                    filter (/= SyncEvent SynReport) evs' @?= init evs
+    listDirectory evdevDir
+        >>= traverse (fmap eitherToMaybe . try @IOError . (retryIf isPermissionError . newDevice) . (evdevDir </>))
+        >>= findM (fmap (== duName) . deviceName) . catMaybes
+        >>= \case
+            Nothing -> assertFailure "Couldn't find device with correct name"
+            Just d -> do
+                putMVar start ()
+                (@?= [EvSyn, EvKey]) =<< deviceEventTypes d
+                evs' <- whileJust ((\x -> guard (x /= last evs) $> x) . eventData <$> nextEvent d) pure
+                filter (/= SyncEvent SynReport) evs' @?= init evs
+
+inverses :: TestTree
+inverses =
+    localOption (QuickCheckTests 1000) . testGroup "Inverses" $
+        [ testGroup
+            "TimeVal"
+            [ testProperty "1" \(s, us) ->
+                let tv = CTimeVal s us
+                 in s < 0 || us < 0 || us >= 1_000_000 || toCTimeVal (fromCTimeVal tv) == tv
+            , testProperty "2" \n ->
+                let -- 'toCTimeVal' goes from picoseconds to microseconds
+                    resolutionFactor = 1_000_000
+                 in abs (diffTimeToPicoseconds (fromCTimeVal . toCTimeVal $ picosecondsToDiffTime n) - n)
+                        < resolutionFactor
+            ]
+        , testProperty "EventData" \x@(t, c, _v) ->
+            let x'@(t', c', v') = toCEventData (fromCEventData x)
+                syncValueZero =
+                    -- 'toCEventData' takes all values for sync events to 0 - fine as they don't mean anything
+                    and
+                        [ t == t'
+                        , fromEnum t == fromEnum EvSyn
+                        , c == c'
+                        , v' == 0
+                        ]
+             in x' == x || syncValueZero
+        ]
 
 --TODO make delay and max retries configurable, add to library?
 retryIf :: forall a e. Exception e => (e -> Bool) -> IO a -> IO a
