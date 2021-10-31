d0c8071a318182a54693c066b709a92c9a96b004
George Thomas <georgefsthomas@gmail.com>
2021-10-31 11:40:38 +0000

    Less laziness in trigger example - fail fast on unparseable key

--- evdev-examples/trigger/Main.hs
+++ evdev-examples/trigger/Main.hs
@@ -11,7 +11,7 @@ import Evdev.Stream
 -- | Run a command on any occurrence of the given key, across all devices.
 main :: IO ()
 main = getArgs >>= \case
-    (read -> key) : p : ps ->
+    (read -> !key) : p : ps ->
         flip S.mapM_ (snd <$> readEventsMany allDevices) \case
             Event{eventData = KeyEvent k Pressed} | k == key ->
                 putStrLn =<< readProcess p ps ""
