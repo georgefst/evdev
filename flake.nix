808c03595d36db554198e5715e2e48a19d4320f3
George Thomas <georgefsthomas@gmail.com>
2026-03-30 21:46:23 +0100

    Add Haskell.nix config

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Basic Haskell flake";
+  inputs.haskell-nix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskell-nix/nixpkgs-2511";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = inputs@{ self, nixpkgs, flake-utils, haskell-nix, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        overlays = [
+          haskell-nix.overlay
+          (final: prev: {
+            myHaskellProject =
+              final.haskell-nix.hix.project {
+                src = ./.;
+                compiler-nix-name = "ghc912";
+                evalSystem = "x86_64-linux";
+                crossPlatforms = p: [ p.aarch64-multiplatform ];
+                shell.tools.cabal = "latest";
+                shell.tools.haskell-language-server = "latest";
+                shell.withHoogle = false;
+              };
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskell-nix) config; };
+      in
+      pkgs.myHaskellProject.flake { });
+}
