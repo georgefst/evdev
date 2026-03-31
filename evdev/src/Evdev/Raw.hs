91532e76cffdf6bcd6ff73068b8bf7059e888374
George Thomas <georgefsthomas@gmail.com>
2026-03-31 10:23:11 +0100

    minimise TH paths

--- evdev/src/Evdev/Raw.hs
+++ evdev/src/Evdev/Raw.hs
@@ -20,11 +20,8 @@ do
             { clang =
                 def
                     { extraIncludeDirs =
-                        [ Dir "/nix/store/iqs23in0fqnf44vnb8l98x7bai77jiv3-libevdev-1.13.4/include"
-                        , Dir "/nix/store/iqs23in0fqnf44vnb8l98x7bai77jiv3-libevdev-1.13.4/include/libevdev-1.0"
-                        , Dir "/nix/store/7iwv8dcgsjmkrnn752hnfdxh3f7wahmd-linux-headers-6.16.7/include"
+                        [ Dir "/nix/store/iqs23in0fqnf44vnb8l98x7bai77jiv3-libevdev-1.13.4/include/libevdev-1.0"
                         , Dir "/nix/store/gi4cz4ir3zlwhf1azqfgxqdnczfrwsr7-glibc-2.40-66-dev/include"
-                        , Dir "/nix/store/kl4w4f8bb77faahsdv40gjmfzg2d081d-clang-21.1.2-lib/lib/clang/21/include"
                         ]
                     }
             , fieldNamingStrategy = OmitFieldPrefixes
@@ -32,7 +29,5 @@ do
             }
         def
         do
-            hashInclude "libevdev-1.0/libevdev/libevdev.h"
-            hashInclude "libevdev-1.0/libevdev/libevdev-uinput.h"
-            hashInclude "linux/input.h"
-            hashInclude "linux/input-event-codes.h"
+            hashInclude "libevdev/libevdev.h"
+            hashInclude "libevdev/libevdev-uinput.h"
