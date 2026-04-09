0b39f87519e691a749510ca0955a0edc827afbdb
George Thomas <georgefsthomas@gmail.com>
2026-04-09 19:40:06 +0100

    format

--- evdev/src/Evdev/Codes.hs
+++ evdev/src/Evdev/Codes.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE TemplateHaskell #-}
 
--- | Datatypes corresponding to the constants in [input-event-codes.h](https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h).
--- See [the Linux Kernel documentation](https://www.kernel.org/doc/html/latest/input/event-codes.html) for full details, noting that all names have been mechanically transformed into CamelCase.
+{- | Datatypes corresponding to the constants in [input-event-codes.h](https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h).
+See [the Linux Kernel documentation](https://www.kernel.org/doc/html/latest/input/event-codes.html) for full details, noting that all names have been mechanically transformed into CamelCase.
+-}
 module Evdev.Codes where
 
 import Evdev.Codes.Generator
