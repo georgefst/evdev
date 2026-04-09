d4f279b32f928dd7e90221f238e685ea4c65221b
George Thomas <georgefsthomas@gmail.com>
2026-04-09 20:44:37 +0100

    vibey use of hs-bindgen Nix hook - works but hard to see how it would work for NixOS users to just get the library straight from Hackage, without adding special support to Haskell.nix etc.

--- evdev/src/Evdev/Codes.hs
+++ evdev/src/Evdev/Codes.hs
@@ -5,24 +5,38 @@ See [the Linux Kernel documentation](https://www.kernel.org/doc/html/latest/inpu
 -}
 module Evdev.Codes where
 
-import Data.Char
+import Control.Applicative
+import Control.Monad
 import Data.List
 import Data.Maybe
-import Data.Tuple.Extra
 import Evdev.Codes.Generator
 import Evdev.Raw
 import Language.Haskell.TH
-import System.Process
+import System.Directory
+import System.Environment
 import Util
 
 $( do
-    libc <-
-        dropWhile isSpace
-            . fromMaybe (error "bad cpp response")
-            . find ("libc" `isInfixOf`)
-            . dropWhile (not . ("#include" `isPrefixOf`))
-            . lines
-            . thd3
-            <$> runIO (readProcessWithExitCode "cpp" ["-v"] "")
-    generateCodes $ libc <> "/linux/input-event-codes.h"
+    -- Find linux/input-event-codes.h. On Nix, we search the include dirs from
+    -- BINDGEN_EXTRA_CLANG_ARGS. On non-Nix, it's in /usr/include.
+    let header = "linux/input-event-codes.h"
+        -- Parse -I, -isystem, and -idirafter flags from clang args.
+        -- These can appear as "-isystem/path" or "-isystem /path" (two tokens).
+        includeDirs = go . words
+          where
+            twoPartFlags = ["-isystem", "-idirafter", "-iprefix", "-iwithprefix"]
+            go [] = []
+            go (flag : dir : rest) | flag `elem` twoPartFlags = dir : go rest
+            go (w : rest)
+                | Just dir <- stripPrefix "-isystem" w <|> stripPrefix "-idirafter" w <|> stripPrefix "-I" w = dir : go rest
+                | otherwise = go rest
+        findHeader dirs = runIO $ listToMaybe <$> filterM (\d -> doesFileExist $ d <> "/" <> header) dirs
+    result <-
+        runIO (lookupEnv "BINDGEN_EXTRA_CLANG_ARGS") >>= \case
+            Just args -> findHeader $ includeDirs args
+            Nothing -> findHeader ["/usr/include"]
+    incDir <- case result of
+        Just d -> pure d
+        Nothing -> error $ "Could not find " <> header <> ". Set BINDGEN_EXTRA_CLANG_ARGS or install linux headers."
+    generateCodes $ incDir <> "/" <> header
  )
